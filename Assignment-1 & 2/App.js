920a9918cdf617515a2f8cef32617d831ef5ecc3
SubhamSahu01 <subhamsahu08092001@gmail.com>
2024-06-23 13:09:39 +0530

    app

---
+++ Assignment-1 & 2/App.js
@@ -0,0 +1,34 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Welcome from './Screens/Welcome';
+import Login from './Screens/Login';
+import Register from './Screens/Register';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+      <NavigationContainer >
+        <Stack.Navigator screenOptions={{headerShown: false}} >
+          <Stack.Screen
+            name="Home"
+            component={Welcome}
+          />
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="Register" component={Register} />
+        </Stack.Navigator>
+      </NavigationContainer>
+
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
